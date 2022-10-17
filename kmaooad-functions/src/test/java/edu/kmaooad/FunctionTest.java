3f9c89485a2f163d914f10e63c071e355ddc30e8
Iryna <irama05@outlook.com>
2022-10-17 19:19:05 +0300

    feat: function firstly covered with tests

--- kmaooad-functions/src/test/java/edu/kmaooad/FunctionTest.java
+++ kmaooad-functions/src/test/java/edu/kmaooad/FunctionTest.java
@@ -17,16 +17,14 @@ import static org.mockito.Mockito.*;
  * Unit test for Function class.
  */
 public class FunctionTest {
-    /**
-     * Unit test for HttpTriggerJava method.
-     */
-    @Test
-    public void testHttpTriggerJava() throws Exception {
+
+    private static final String BAD_RESPONSE = "Request body must not be blank and must have message object with message_id field";
+
+    private HttpRequestMessage<Optional<String>> getReq(Optional<String> queryBody) {
         // Setup
         @SuppressWarnings("unchecked")
         final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
 
-        final Optional<String> queryBody = Optional.of("Azure");
         doReturn(queryBody).when(req).getBody();
 
         doAnswer(new Answer<HttpResponseMessage.Builder>() {
@@ -37,13 +35,80 @@ public class FunctionTest {
             }
         }).when(req).createResponseBuilder(any(HttpStatus.class));
 
+        return req;
+    }
+
+    private ExecutionContext getContext() {
+        // Setup
         final ExecutionContext context = mock(ExecutionContext.class);
         doReturn(Logger.getGlobal()).when(context).getLogger();
-
-        // Invoke
-        final HttpResponseMessage ret = new Function().run(req, context);
-
-        // Verify
-        assertEquals(ret.getStatus(), HttpStatus.BAD_REQUEST);
+        return context;
     }
+
+    @Test
+    public void testFunctionWithoutBody() {
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.empty()), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithWrongStringInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("Azure")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithMessageWithoutMessageIdInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {}\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithMessageIdWithoutMessageInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message_id\": 12345\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithWrongTypeOfMessageIdInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {\n" +
+                "        \"message_id\": some_id\n" +
+                "    }\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithCorrectBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {\n" +
+                "        \"message_id\": 12345\n" +
+                "    }\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.OK, ret.getStatus());
+        assertEquals("message_id=12345", ret.getBody());
+    }
+
 }
