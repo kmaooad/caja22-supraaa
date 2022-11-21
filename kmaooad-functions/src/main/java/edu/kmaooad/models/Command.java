b3eb0dc1206a316ff9ba95d6d688f6e6367b5d9d
--replace-all <dgosan37@gmail.com>
2022-11-21 21:24:15 +0200

    add: command entity

---
+++ kmaooad-functions/src/main/java/edu/kmaooad/models/Command.java
@@ -0,0 +1,22 @@
+package edu.kmaooad.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class Command {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+}
