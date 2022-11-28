dee94e184473829dd79c4b98bf471040bb478486
Iryna <irama05@outlook.com>
2022-11-28 19:34:02 +0200

    feat: split BanService to three separate services

--- kmaooad-functions/src/test/java/edu/kmaooad/BanTest.java
+++ kmaooad-functions/src/test/java/edu/kmaooad/BanTest.java
@@ -1,6 +1,8 @@
 package edu.kmaooad;
 
-import edu.kmaooad.services.interfaces.BanService;
+import edu.kmaooad.services.interfaces.BanDepartmentService;
+import edu.kmaooad.services.interfaces.BanOrganizationService;
+import edu.kmaooad.services.interfaces.BanUserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,42 +14,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BanTest {
 
     @Autowired
-    private BanService banService;
+    private BanUserService banUserService;
+    @Autowired
+    private BanDepartmentService banDepartmentService;
+    @Autowired
+    private BanOrganizationService banOrganizationService;
 
     @Test
     public void banForUserShouldBeAdded() {
-        banService.addBanForUser(1L);
-        assertTrue(banService.isUserBanned(1L));
+        banUserService.addBanForUser(1L);
+        assertTrue(banUserService.isUserBanned(1L));
     }
 
     @Test
     public void banForUserShouldBeDeleted() {
-        banService.deleteBanForUser(1L);
-        assertFalse(banService.isUserBanned(1L));
+        banUserService.deleteBanForUser(1L);
+        assertFalse(banUserService.isUserBanned(1L));
     }
 
     @Test
     public void banForDepartmentShouldBeAdded() {
-        banService.addBanForDepartment(1L);
-        assertTrue(banService.isDepartmentBanned(1L));
+        banDepartmentService.addBanForDepartment(1L);
+        assertTrue(banDepartmentService.isDepartmentBanned(1L));
     }
 
     @Test
     public void banForDepartmentShouldBeDeleted() {
-        banService.deleteBanForDepartment(1L);
-        assertFalse(banService.isDepartmentBanned(1L));
+        banDepartmentService.deleteBanForDepartment(1L);
+        assertFalse(banDepartmentService.isDepartmentBanned(1L));
     }
 
     @Test
     public void banForOrganizationShouldBeAdded() {
-        banService.addBanForOrganization(1L);
-        assertTrue(banService.isOrganizationBanned(1L));
+        banOrganizationService.addBanForOrganization(1L);
+        assertTrue(banOrganizationService.isOrganizationBanned(1L));
     }
 
     @Test
     public void banForOrganizationShouldBeDeleted() {
-        banService.deleteBanForOrganization(1L);
-        assertFalse(banService.isOrganizationBanned(1L));
+        banOrganizationService.deleteBanForOrganization(1L);
+        assertFalse(banOrganizationService.isOrganizationBanned(1L));
     }
 
 }
