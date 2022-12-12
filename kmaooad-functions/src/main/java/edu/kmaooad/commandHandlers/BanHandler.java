46c425e4b2b940bca7ed4a84674dce5d8ae01bb1
vdzhos <dgosan37@gmail.com>
2022-12-12 21:52:33 +0200

    fixed: ban and unban handlers to use separated ban services

--- kmaooad-functions/src/main/java/edu/kmaooad/commandHandlers/BanHandler.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/commandHandlers/BanHandler.java
@@ -3,7 +3,9 @@ package edu.kmaooad.commandHandlers;
 import edu.kmaooad.events.HandlerEvent;
 import edu.kmaooad.models.IssuerType;
 import edu.kmaooad.processing.CommandCall;
-import edu.kmaooad.services.interfaces.BanService;
+import edu.kmaooad.services.interfaces.BanDepartmentService;
+import edu.kmaooad.services.interfaces.BanOrganizationService;
+import edu.kmaooad.services.interfaces.BanUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,9 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class BanHandler implements CommandHandler{
 
-    private BanService banService;
+    private BanUserService banUserService;
+    private BanDepartmentService banDepartmentService;
+    private BanOrganizationService banOrganizationService;
     private ApplicationEventPublisher applicationEventPublisher;
 
     /*
@@ -32,26 +36,26 @@ public class BanHandler implements CommandHandler{
         switch (issuerType) {
             case USER:
                 issuer = "User";
-                if(banService.isUserBanned(issuerId)) {
+                if(banUserService.isUserBanned(issuerId)) {
                     throw new Exception("This user is already banned!");
                 } else {
-                    banService.addBanForUser(issuerId);
+                    banUserService.addBanForUser(issuerId);
                 }
                 break;
             case DEPARTMENT:
                 issuer = "Department";
-                if(banService.isDepartmentBanned(issuerId)) {
+                if(banDepartmentService.isDepartmentBanned(issuerId)) {
                     throw new Exception("This department is already banned!");
                 } else {
-                    banService.addBanForDepartment(issuerId);
+                    banDepartmentService.addBanForDepartment(issuerId);
                 }
                 break;
             case ORGANIZATION:
                 issuer = "Organization";
-                if(banService.isOrganizationBanned(issuerId)) {
+                if(banOrganizationService.isOrganizationBanned(issuerId)) {
                     throw new Exception("This organization is already banned!");
                 } else {
-                    banService.addBanForOrganization(issuerId);
+                    banOrganizationService.addBanForOrganization(issuerId);
                 }
                 break;
         }
--- kmaooad-functions/src/main/java/edu/kmaooad/commandHandlers/UnbanHandler.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/commandHandlers/UnbanHandler.java
@@ -3,7 +3,9 @@ package edu.kmaooad.commandHandlers;
 import edu.kmaooad.events.HandlerEvent;
 import edu.kmaooad.models.IssuerType;
 import edu.kmaooad.processing.CommandCall;
-import edu.kmaooad.services.interfaces.BanService;
+import edu.kmaooad.services.interfaces.BanDepartmentService;
+import edu.kmaooad.services.interfaces.BanOrganizationService;
+import edu.kmaooad.services.interfaces.BanUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,9 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class UnbanHandler implements CommandHandler{
 
-    private BanService banService;
+    private BanUserService banUserService;
+    private BanDepartmentService banDepartmentService;
+    private BanOrganizationService banOrganizationService;
     private ApplicationEventPublisher applicationEventPublisher;
 
     /*
@@ -32,26 +36,26 @@ public class UnbanHandler implements CommandHandler{
         switch (issuerType) {
             case USER:
                 issuer = "User";
-                if(!banService.isUserBanned(issuerId)) {
+                if(!banUserService.isUserBanned(issuerId)) {
                     throw new Exception("This user is not banned!");
                 } else {
-                    banService.deleteBanForUser(issuerId);
+                    banUserService.deleteBanForUser(issuerId);
                 }
                 break;
             case DEPARTMENT:
                 issuer = "Department";
-                if(!banService.isDepartmentBanned(issuerId)) {
+                if(!banDepartmentService.isDepartmentBanned(issuerId)) {
                     throw new Exception("This department is not banned!");
                 } else {
-                    banService.deleteBanForDepartment(issuerId);
+                    banDepartmentService.deleteBanForDepartment(issuerId);
                 }
                 break;
             case ORGANIZATION:
                 issuer = "Organization";
-                if(!banService.isOrganizationBanned(issuerId)) {
+                if(!banOrganizationService.isOrganizationBanned(issuerId)) {
                     throw new Exception("This organization is not banned!");
                 } else {
-                    banService.deleteBanForOrganization(issuerId);
+                    banOrganizationService.deleteBanForOrganization(issuerId);
                 }
                 break;
         }
