9922bcaa6be6547c20c5dace0315fb85277ed118
vdzhos <dgosan37@gmail.com>
2022-12-13 03:13:28 +0200

    add: tests for ban handler and unban handler

---
+++ kmaooad-functions/src/test/java/edu/kmaooad/UnbanHandlerTest.java
@@ -0,0 +1,74 @@
+package edu.kmaooad;
+
+import edu.kmaooad.commandHandlers.BanHandler;
+import edu.kmaooad.commandHandlers.UnbanHandler;
+import edu.kmaooad.processing.CommandCall;
+import edu.kmaooad.services.interfaces.BanDepartmentService;
+import edu.kmaooad.services.interfaces.BanOrganizationService;
+import edu.kmaooad.services.interfaces.BanUserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest
+public class UnbanHandlerTest {
+
+    @Autowired
+    private UnbanHandler unbanHandler;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        @Primary
+        public BanUserService banUserService() {
+            return mock(BanUserService.class);
+        }
+
+        @Primary
+        public BanDepartmentService banDepartmentService() {
+            return mock(BanDepartmentService.class);
+        }
+
+
+        @Primary
+        public BanOrganizationService banOrganizationService() {
+            return mock(BanOrganizationService.class);
+        }
+
+        @Bean
+        @Primary
+        public ApplicationEventPublisher applicationEventPublisher() {
+            return mock(ApplicationEventPublisher.class);
+        }
+
+    }
+
+    @Test
+    public void givenCorrectCommandCall_whenHandle_shouldThrowBecauseNoSuchUser() {
+        CommandCall commandCall = new CommandCall(0L, 123L, 456L, new String[]{"USER", "0"});
+        assertThrows(Exception.class, () -> unbanHandler.handle(commandCall));
+    }
+
+    @Test
+    public void givenCommandCall_whenArgAbsentAndHandle_shouldThrow() {
+        CommandCall commandCall = new CommandCall(0L, 123L, 456L, new String[]{"USER"});
+        assertThrows(Exception.class, () ->  unbanHandler.handle(commandCall));
+
+    }
+
+    @Test
+    public void givenCommandCall_whenArgTypeIncorrectAbsentAndHandle_shouldThrow() {
+        CommandCall commandCall = new CommandCall(0L, 123L, 456L, new String[]{"INCORRECT_TYPE", "0"});
+        assertThrows(Exception.class, () ->  unbanHandler.handle(commandCall));
+    }
+
+}
